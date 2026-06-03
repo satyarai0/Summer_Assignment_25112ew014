d21a2dc5f226c477bba6a4fa13df1116dba4eeab
Satya Rai <raisatya360@gmail.com>
2026-06-03 18:28:17 +0530

    day 1 assignment complete

---
+++ day_1/Q1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int sum = 0; 
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++) {
+        sum = sum + i; 
+
+    }
+
+    printf("Sum is: %d\n", sum);
+
+    return 0;
+}
